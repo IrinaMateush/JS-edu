f439cd43a671304ba6fb4a815397d92cfdc1baa6
Irina Mateush <ismateush@gmail.com>
2017-11-20 16:46:10 +0300

    работа с массивом

--- object.js
+++ object.js
@@ -3,7 +3,7 @@
 
 //this использ для доступа к собственным данным объекта в котором определена вызванная функция
 //иначе говоря спец переменная которая обозначает сам объект
-//в this идет то значение, которое слева от точки
+//в this идет то значение, которое слева от точки во время вызова функции 
 
 const user = {
     userName: "Tim",
@@ -23,6 +23,29 @@ const user = {
 user.sayHello("Nick");
 
 let data = user.data; //можно записывать в ссылку
-data.showMessage("hello!")
+data.showMessage("hello!") // слева от точки в вызове ф-ции data => data пойдет в this
 
+//нельзя завести объект и перебрать ео свойства с помощью for of или for each. Как делать?
 
+const user = {
+    userName: "Tim",
+    userAge: 29,
+    isAdmin: true,
+    complicated: {
+        foo: "bar"
+    }
+};
+
+for(let field of Object.keys(user)){
+    console.log(field); //выведет массив ключей объекта
+}
+for(let field of Object.values(user)){
+    console.log(field); //выведет массив значений объекта
+}
+for(let field of Object.entries(user)){
+    console.log(field); //выведет массив массивов ключ - значение объекта ["name", "Tim"] ... 
+}
+// деструктуризация массива. Вместо field объявляем структуру, тогда ответ будет не массивом (без квадратных скобок)
+for(let [key, value] of Object.entries(user)){
+    console.log(key, value); //выведет ключ - значение объекта "name", "Tim" ... 
+}
