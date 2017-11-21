2393d51919918d7e6354d847d25c17a5f4514dd2
Irina Mateush <ismateush@gmail.com>
2017-11-21 16:48:49 +0300

    вывод списка друзей

---
+++ friends.js
@@ -0,0 +1,20 @@
+"use strict";
+
+//вывести список друзей на страницу 
+
+const friends = ["Tim","Olga","Max","Egor"];
+
+function printFriends (sourceEl) {
+    let ul = document.createElement('ul');
+
+
+    friends.forEach(friendName => {
+        let li = document.createElement('li');
+        li.innerText = friendName;
+        ul.append(li);
+    });
+    sourceEl.append(ul);
+}
+
+printFriends(document.querySelector(".js-friends"));
+
