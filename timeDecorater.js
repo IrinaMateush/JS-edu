06d2b6877c0e7455ea37f3206d132b6b31ae6224
Irina Mateush <ismateush@gmail.com>
2017-12-08 15:39:11 +0300

    time decorator точнее и с аргументами

--- timeDecorater.js
+++ timeDecorater.js
@@ -1,17 +1,23 @@
 "use strict";
 // написать декоратор, который выдает время работы функции 
 
-function foo () {
-    console.log("foo");
+function foo (max = 10) {
+    return 2 ** max;
 }
 
+let some = foo(100);
+console.log(some);
+
 function timeDecorator(func){
-    let current = Date.now();
-    return function(){
-        func();
-        console.log(Date.now() - current);
+    return function(...args){
+        let current = performance.now();
+        let result = func.apply(this, args);
+
+        console.log('Time:', performance.now() - current);
+        return result;
     };
 }
 
 let timeLogFoo = timeDecorator(foo);
+some = timeLogFoo(10);
 timeLogFoo();
